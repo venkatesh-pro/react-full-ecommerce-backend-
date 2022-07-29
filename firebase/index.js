e556a42e8b818b1448db23c19e01f761a603aee9
Venkatesh <74946135+venkatesh-pro@users.noreply.github.com>
2022-07-29 08:55:06 +0530

    Update index.js

--- firebase/index.js
+++ firebase/index.js
@@ -4,7 +4,7 @@ var serviceAccount = require("../config/fbServiceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://ecommerce-225c8.firebaseio.com",
+  databaseURL: "",
 });
 
 module.exports = admin;
